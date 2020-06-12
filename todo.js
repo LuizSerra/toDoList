ebfbbbe34f123156d7725e24ebe98b78248c0477
Luiz Serra <lserra.trade@gmail.com>
2020-06-12 17:20:04 -0300

    passando alguns controles para manipulação via JS

--- todo.js
+++ todo.js
@@ -1,1 +1,9 @@
-alert('Funcionou!');
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer Café.',
+    'Estudar JavaScript.',
+    'Acessar Conteúdo de Estudo.',
+]
