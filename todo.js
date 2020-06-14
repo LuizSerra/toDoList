a6070215562d08921a24c654d0ff319a64233912
Luiz Serra <lserra.trade@gmail.com>
2020-06-13 21:41:37 -0300

    adicionando comportamento de adição de todo

--- todo.js
+++ todo.js
@@ -8,11 +8,27 @@ var todos = [
     'Acessar Conteúdo de Estudo.',
 ]
 
-for (todo of todos) {
-    var todoElement = document.createElement('li');
-    var todoText = document.createTextNode(todo);
+function renderTodos() {
+    listElement.innerHTML = '';
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
 
-    todoElement.appendChild(todoText);
-    listElement.appendChild(todoElement);
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
 
+    }
 }
+
+renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+    if (todoText != '') {
+        todos.push(todoText);
+        inputElement.value = '';
+        renderTodos();
+    }
+}
+
+buttonElement.onclick = addTodo;
